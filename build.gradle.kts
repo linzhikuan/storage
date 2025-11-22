29fe60494ff6ea07686479b4d0bb90f4225286dc
linzhikuan <1065627939@qq.com>
2025-11-22 18:31:53 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+// 应用通用库配置
+apply(from = "${rootProject.projectDir}/gradles/library.gradle")
+
+android {
+    namespace = "com.lzk.demo.core.storage"
+}
+
+dependencies {
+    // DataStore dependencies
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+}
